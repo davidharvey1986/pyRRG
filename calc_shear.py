dfdae747d5beb67dfa93a6469cc7397bc36a5e82
davidharvey1986 <davidharvey1986@googlemail.com>
2016-11-28 16:45:14 +0100

    Documentation added

--- calc_shear.py
+++ calc_shear.py
@@ -2,20 +2,49 @@ import numpy as np
 import ipdb as pdb
 import pyfits as py
 
-def calc_shear( corrected_moments, galaxies,
-                outfile,
-                 min_rad=6, mult=2,
-                 size_cut_lo=0.,
-                 size_cut_hi=100.,
-                 mag_cut_lo=22.5,
-                 mag_cut_hi=30,
-                 signal_noise_cut=3.,
-                 rhodes_factor=0.86,
-                 dataDir='./',
-                 expThresh=4,
-                 stat_type='median'):
+def calc_shear( corrected_moments, galaxies, outfile,
+                min_rad=6, mult=2,
+                size_cut_lo=0.,
+                size_cut_hi=100.,
+                mag_cut_lo=22.5,
+                mag_cut_hi=30,
+                signal_noise_cut=3.,
+                rhodes_factor=0.86,
+                dataDir='./',
+                expThresh=4,
+                stat_type='median'):
+    '''
+    PURPOSE : TO WORK OUT VARIOUS FACTORS AND ADJUST THE PSF
+    CORRECTED ELLIPICITY FOR THESE AND GET AN ESTAIMTE OF THE SHEAR
 
 
+    INPUT  :
+        CORRECTED_MOMENTS : A fitsrec structure of the corrected
+                            and uncorrected moments to measure
+                            and estimate the shear.
+        GALAXIES : A integer vector of indexes that correspond to those
+                   CORRECTED_MOMENTS that have been classified as a galaxy
+        OUTFILE : The name of the fits file in which the shears, gamma1 and gamma2
+                have been estimated as
+
+
+    OPTIONAL INPUTS:
+         MIN_RAD: The minimum measured radios that is used to weight each galaxies in the
+                  measurement.
+        SIZE_CUT_[LO,HI] : scalar, the cut in galaxy size that is allowed to measure shear on
+        MAG_CUT_[LO,HI] : scalar, the cut in the galaxy magnitude that is allowed to measure shear on
+        SIGNAL_NOSIE_CUT : scalar, the lowest acceptable signal to nosie allowed in the catalogue
+        rhodes_factor : The RRG rhodes factor (see paper)
+        dataDir : the directory in which all the data exists
+        expThresh : the minimum number of exposures a galaxy has to have an acceptably robust shape.
+        stat_type :  the stat type used over the field of galaxies to estimate the corrections to
+                     from elliptcitiy to shear.
+        
+    OUTPUT :
+         OUTFILE : A fitsfile named outfile with the same fields as corrected_moments, except with
+                   two extra fields. GAMMA1 and GAMMA2, the two componenets of the estimated shear.
+
+    '''
     #Need to filter as i determine mean quantities that
     #shoudlnt be used from bad galaxies
     signal_noise = corrected_moments['FLUX_AUTO'] / \
@@ -29,20 +58,20 @@ def calc_shear( corrected_moments, galaxies,
           
     good = np.zeros(len(corrected_moments.x))
     good[ galaxies ] = 1
-    #good[ (corrected_moments.xy < 0 ) ]  = 0
+    good[ (corrected_moments.xy < 0 ) ]  = 0
     good[ (corrected_moments.xx + corrected_moments.yy < 0)] = 0
     good[ (corrected_moments.xx < 0)]  = 0
     good[ (corrected_moments.yy < 0 ) ]  = 0
     good[ (uncut_ell_sqr > 2 ) ]  = 0
     good[ (uncor_size < size_cut_lo )]  = 0 
     good[ (uncor_size > size_cut_hi )]  = 0
-    #good[( corrected_moments.MAG_AUTO < mag_cut_lo )]  = 0 
-    #good[( corrected_moments.MAG_AUTO > mag_cut_hi )]  = 0
+    good[( corrected_moments.MAG_AUTO < mag_cut_lo )]  = 0 
+    good[( corrected_moments.MAG_AUTO > mag_cut_hi )]  = 0
     good[ (signal_noise < signal_noise_cut)]  = 0 
     
-    #good[ corrected_moments.nExposures < expThresh ] = 0
-    #good[  (~np.isfinite(corrected_moments.xx)) ] = 0
-    #good[  corrected_moments.prob != 0 ] = 0
+    good[ corrected_moments.nExposures < expThresh ] = 0
+    good[  (~np.isfinite(corrected_moments.xx)) ] = 0
+    good[  corrected_moments.prob != 0 ] = 0
 
     
     momc = corrected_moments[good == 1]
