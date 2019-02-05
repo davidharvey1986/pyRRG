307e46a677f7aca67ecd7c2d7d2119679ba79741
davidharvey1986 <davidharvey1986@googlemail.com>
2019-02-05 11:25:11 -0800

    Training some more

--- trainStarGalClass/trainBestParamsSVM.py
+++ trainStarGalClass/trainBestParamsSVM.py
@@ -24,7 +24,7 @@ def gridSearch():
       
     params_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
           'gamma': [0.0001, 0.001, 0.01, 0.1],
-          'kernel':['linear','rbf'] }
+          'kernel':['linear','rbf','poly'] }
 
     
     #Create the GridSearchCV object
