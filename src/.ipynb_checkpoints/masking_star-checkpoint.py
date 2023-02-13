masking star bug

src/.ipynb_checkpoints/masking_star-checkpoint.py
@@ -2,7 +2,6 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import os as os
-import ipdb as pdb
 import RRGtools as tools
 from .getIndividualExposures import getIndividualExposures
 from .acs_limits import acs_limits
@@ -159,7 +158,7 @@ def main(  shear_catalog, object_catalog_fits, \
          **kwargs):
     
     main_single(shear_catalog, object_catalog_fits, \
-         mask_file='star_masks.reg', outFile=outFile, 
+         mask_file=mask_file, outFile=outFile, 
          mask_stars=True,  plot_reg=None)
     
     return
@@ -246,6 +245,7 @@ def main_single(  shear_catalog, object_catalog_fits, \
 
     ##########plot remove_star.reg---------------------------------------------------------
     star_corr=[[] for i in np.arange(len(Star_catalogue["ra"]))]
+    print("Masking")
     for j in np.arange(len(Star_catalogue["ra"])):
         star_x=Star_catalogue["X_IMAGE"][j]
         star_y=Star_catalogue["Y_IMAGE"][j]
@@ -260,7 +260,7 @@ def main_single(  shear_catalog, object_catalog_fits, \
     if mask_stars:
         Shears=fits.open(clean_catalog)[1].data
     ##go through the sources list:
-        for i in np.arange(len(Shears['ra'])):
+        for i in tqdm.tqdm(np.arange(len(Shears['ra']))):
             xl=Shears['X_IMAGE'][i]
             yl=Shears["Y_IMAGE"][i]
             for j in np.arange(len(Star_catalogue["ra"])):   #go through the star list
@@ -292,12 +292,12 @@ def main_single(  shear_catalog, object_catalog_fits, \
                 continue
             elif mask[0:3] == 'box':
                 print("masking a box")
-                mask_x = np.float(mask.split('(')[1].split(',')[0])
-                mask_y = np.float(mask.split('(')[1].split(',')[1])
-                mask_sizex = np.float(mask.split('(')[1].split(',')[2][:-1])
-                mask_sizey = np.float(mask.split('(')[1].split(',')[3][:-1])
+                mask_x = float(mask.split('(')[1].split(',')[0])
+                mask_y = float(mask.split('(')[1].split(',')[1])
+                mask_sizex = float(mask.split('(')[1].split(',')[2][:-1])
+                mask_sizey = float(mask.split('(')[1].split(',')[3][:-1])
                 
-                mask_angle = np.float(mask.split('(')[1].split(',')[4][:-2])
+                mask_angle = float(mask.split('(')[1].split(',')[4][:-2])
                 #rotate the shears into fram of reference of the mask
                 shears_x_mask_ref = tools.ra_separation(Shears_remove['X_WORLD'], mask_y, mask_x , mask_y)
                 shears_y_mask_ref = (Shears_remove['Y_WORLD'] - mask_y)*3600.

src/rrg_to_lenstool.py
@@ -46,13 +46,14 @@ def rrg_to_lenstool( rrg_catalogue,
     rotang = image[rrgParams['fits_extension']].header[rrgParams['orientation_header']]
     theta += rotang
     #print image[0].header['ORIENTAT']
-    gamma = 2.*np.sqrt(  MaskedRRGCat.gamma2**2 + MaskedRRGCat.gamma1**2)
+    gamma = np.sqrt(  MaskedRRGCat.gamma2**2 + MaskedRRGCat.gamma1**2)
 
-    size = np.sqrt(( MaskedRRGCat.xx + MaskedRRGCat.yy)/2.)/2. * \
+    #For a2-b^2 ellipticuity e= e* + 2g
+    size = 2.*np.sqrt(( MaskedRRGCat.xx + MaskedRRGCat.yy)/2.) * \
         image[rrgParams['fits_extension']].header['CD2_2']*3600.
 
-    semi_major = size*(1.+gamma) ##RRG returns shear(gamma) and lenstool defines a=sqrt(1+e),b=sqrt(1-e), where e~2gamma. Thus,we define a=(1.+gamma) and b=(1.-gamma) here to match the definition of lenstool input.    
-    semi_minor = size*(1.-gamma)
+    semi_major = size*np.sqrt(1.+gamma) ##RRG returns shear(gamma) and lenstool defines a=sqrt(1+e),b=sqrt(1-e), where e~2gamma. Thus,we define a=(1.+gamma) and b=(1.-gamma) here to match the definition of lenstool input.    
+    semi_minor = size*np.sqrt(1.-gamma)
 
     if reference is None:
         reference = [ np.mean( MaskedRRGCat.RA ), 